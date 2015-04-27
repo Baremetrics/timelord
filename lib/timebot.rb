f53a59c896176dc57dc90b36aba53f8ec45da054
Ben Sharpe <bsharpe@gmail.com>
2015-04-27 11:05:18 -0600

    add get

--- lib/timebot.rb
+++ lib/timebot.rb
@@ -18,6 +18,30 @@ TIME_ZONES = [
   'US/Eastern'
 ]
 
+def do_times(phrase)
+  message = nil
+  emoji = nil
+  begin
+    Time.zone = "UTC"
+    Chronic.time_class = Time.zone
+    time = Chronic.parse(phrase)
+    if time
+      times = []
+      TIME_ZONES.each do |zone|
+        z = TZInfo::Timezone.get(zone)
+        times << time.in_time_zone(z).strftime('%I:%M%P')
+      end
+      message = "> #{times.join(' | ')}"
+      h = time.strftime('%I')
+      h = h[1] if h.start_with?('0')
+      emoji = ":clock#{h}:"
+    end
+    [message, emoji]
+  rescue => e
+    p e.message
+    [nil, nil]
+  end
+end
 
 module TimeBot
   class Web < Sinatra::Base
@@ -26,28 +50,16 @@ module TimeBot
       return 401 unless request["token"] == ENV['SLACK_TOKEN']
     end
 
+    get '/time' do
+      message, emoji = do_times(params[:phrase])
+      status 200
+      
+      reply = { username: 'timelord', icon_emoji: emoji, text: message } 
+      return reply.to_json
+    end
+    
     post "/time" do
-      message = nil
-      emoji = nil
-      begin
-        Time.zone = "UTC"
-        Chronic.time_class = Time.zone
-        time = Chronic.parse(request['text'])
-        if time
-          times = []
-          TIME_ZONES.each do |zone|
-            z = TZInfo::Timezone.get(zone)
-            times << time.in_time_zone(z).strftime('%I:%M%P')
-          end
-          message = "> #{times.join(' | ')}"
-          h = time.strftime('%I')
-          h = h[1] if h.start_with?('0')
-          emoji = ":clock#{h}:"
-        end
-      rescue => e
-        p e.message
-        halt
-      end
+      message, emoji = do_times(request['text'])
       status 200
       
       reply = { username: 'timelord', icon_emoji: emoji, text: message } 
